4668a600be985a180b3c8fff444cdf5f78b58260
Sai Harish Sarma <99181593+saiharishsarma@users.noreply.github.com>
2023-10-23 18:23:12 +0530

    CustomerDataSimulation-BusinessAnalysisTool-Code.py

--- CustomerDataSimulation-BusinessAnalysisTool-Code.py
+++ CustomerDataSimulation-BusinessAnalysisTool-Code.py
@@ -33,6 +33,7 @@ for i in range(int(nums)):
 
 
 # Creating Dataframe of the created sample dataset
+columns= ['CustomerID','VisitDate', 'TotalSpend', 'ProductCategory']
 df= pd.DataFrame(data,  columns= ['CustomerID','VisitDate', 'TotalSpend', 'ProductCategory'])
 
 
@@ -62,68 +63,69 @@ st.title("B. Customer Behaviour Analysis and Suggestion Tool")
 
 # Creating Interface to upload CSV dataset file to get analysis and suggestions.
 uploaded_file = st.file_uploader("**Upload your CSV file here**", type = ['csv'])
+if uploaded_file:  
+    data= pd.read_csv(uploaded_file, encoding= 'unicode_escape')
 
-
-if list(df.columns) == list(data.columns):
-
-    data= pd.read_csv(uploaded_file)
-    # Basic EDA
+    if list(data.columns) == ['CustomerID','VisitDate', 'TotalSpend', 'ProductCategory']:
     
-    st.subheader("Basic Exploratory Data Analysis of the Dataset:", divider= "rainbow")
-    st.write(f"Size of the Dataset: {data.shape}")
-    st.write("*First 5 rows of the dataset:*")
-    st.write(data.head())
-    st.write("*Lst 5 rows of the dataset:*")
-    st.write(data.tail())
-    st.write("*Statistical Summary of the dataset:*")
-    st.write(data.describe())
-    st.write("*No of Unique entries in each column:*")
-    st.write(data.nunique())
-    st.write("*Check for missing values:*")
-    st.write(data.isnull().sum())
+
+        # Basic EDA
+    
+        st.subheader("Basic Exploratory Data Analysis of the Dataset:", divider= "rainbow")
+        st.write(f"Size of the Dataset: {data.shape}")
+        st.write("*First 5 rows of the dataset:*")
+        st.write(data.head())
+        st.write("*Lst 5 rows of the dataset:*")
+        st.write(data.tail())
+        st.write("*Statistical Summary of the dataset:*")
+        st.write(data.describe())
+        st.write("*No of Unique entries in each column:*")
+        st.write(data.nunique())
+        st.write("*Check for missing values:*")
+        st.write(data.isnull().sum())
 
 
 
   
-    # Extracting KPIs
+        # Extracting KPIs
 
-    AVG_Spent_Per_Customer = round(data['TotalSpend'].mean(), 2)
-    MIN_Spent_Per_Customer = round(data['TotalSpend'].min(), 2)
+        AVG_Spent_Per_Customer = round(data['TotalSpend'].mean(), 2)
+        MIN_Spent_Per_Customer = round(data['TotalSpend'].min(), 2)
 
-    Popular_Product_Category = data['ProductCategory'].mode()[0]
+        Popular_Product_Category = data['ProductCategory'].mode()[0]
 
-    Monthly_AVG_Revenue_Growth= round(data.groupby(data['VisitDate'].str[0:7])['TotalSpend'].sum().mean(),2)
-    Monthly_MIN_Revenue_Growth= round(data.groupby(data['VisitDate'].str[0:7])['TotalSpend'].sum().min(),2)
+        Monthly_AVG_Revenue_Growth= round(data.groupby(data['VisitDate'].str[0:7])['TotalSpend'].sum().mean(),2)
+        Monthly_MIN_Revenue_Growth= round(data.groupby(data['VisitDate'].str[0:7])['TotalSpend'].sum().min(),2)
 
-    # Creating Suggestions to be appeared.
+        # Creating Suggestions to be appeared.
 
-    Suggestions = []
-    if AVG_Spent_Per_Customer < MIN_Spent_Per_Customer:
-        Suggestions.append("Target Low-Spending Customers by personalized recommendations to increase their spend")
+        Suggestions = []
+        if AVG_Spent_Per_Customer < MIN_Spent_Per_Customer:
+            Suggestions.append("Target Low-Spending Customers by personalized recommendations to increase their spend")
 
-    if Popular_Product_Category:
-        Suggestions.append(f"Launch More promotions for {Popular_Product_Category} category to boost the sales")
+        if Popular_Product_Category:
+            Suggestions.append(f"Launch More promotions for {Popular_Product_Category} category to boost the sales")
 
-    if Monthly_AVG_Revenue_Growth < Monthly_MIN_Revenue_Growth:
-        Suggestions.append("Attract More Customers by offering Discounts")
-
-
-    # Adding KPIs Information to the Streamlit dashboard
-    st.subheader("**Key Performance Indicators (KPIs):**", divider='rainbow')
-
-    st.metric("**1. Average Money Spent Per Customer(in USD):**", AVG_Spent_Per_Customer)
-
-    st.metric("**2. Most Popular Product Category:** ", Popular_Product_Category)
-
-    st.metric("**3. Monthly Revenue Growth(in USD):** ", Monthly_AVG_Revenue_Growth)
+        if Monthly_AVG_Revenue_Growth < Monthly_MIN_Revenue_Growth:
+            Suggestions.append("Attract More Customers by offering Discounts")
 
 
-    # Adding Suggestions to the Streamlit Dashboard
-    st.subheader("Suggestions: ", divider= "rainbow")
-    for i in Suggestions:
-        st.markdown(f"***{i}**")
-else:
-    st.warning("This dataset is not useful in this analysis. Kindly upload the correct file!")
+        # Adding KPIs Information to the Streamlit dashboard
+        st.subheader("**Key Performance Indicators (KPIs):**", divider='rainbow')
+
+        st.metric("**1. Average Money Spent Per Customer(in USD):**", AVG_Spent_Per_Customer)
+
+        st.metric("**2. Most Popular Product Category:** ", Popular_Product_Category)
+
+        st.metric("**3. Monthly Revenue Growth(in USD):** ", Monthly_AVG_Revenue_Growth)
+
+
+        # Adding Suggestions to the Streamlit Dashboard
+        st.subheader("Suggestions: ", divider= "rainbow")
+        for i in Suggestions:
+            st.markdown(f"***{i}**")
+    else:
+        st.warning("This dataset is not useful in this analysis. Kindly upload the correct file!")
 
 
     
@@ -131,9 +133,3 @@ st.success(":wave: Thanks for using! Have a nice day. :wave:")
 
 
 
-    
-    
-
-    
-
-    
